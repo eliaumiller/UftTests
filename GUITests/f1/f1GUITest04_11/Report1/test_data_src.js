52a1053ac9ee85c71dd10f6c3f52666a6233a66e
Eli Miller <emiller@microfocus.com>
2023-01-31 17:06:06 +0200

    ssssssssssss

---
+++ GUITests/f1/f1GUITest04_11/Report1/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'D:\\work\\mqm\\jenkins_tickets\\2027086 - Renaming tests in bulk in GIT is not reflected in Octane\\UFTTests\\UftTests\\GUITests\\f1\\GUITest04_2',namePairs:[]}]};
